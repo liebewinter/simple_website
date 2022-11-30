8f342dcd6853e2f2f7c5bd9231078b6a6ce261c4
liebewinter <winter161@hotmail.com>
2022-11-30 20:43:03 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Navbar with site-switch
+Click buttons to switch the sites
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
